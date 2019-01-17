0a64d45b6961f7849699c36cad6610af25186bf0
David Jones <davidjones@sportronics.com.au>
2019-01-17 13:01:00 +1100

    Added args to loglast scripts

--- iotedge/loglast30sec.ps1
+++ iotedge/loglast30sec.ps1
@@ -1,4 +1,4 @@
-Get-WinEvent -ea SilentlyContinue -FilterHashtable @{ProviderName='iotedged';LogName='application';StartTime=[datetime]::Now.AddSeconds(-30)} |
+Get-WinEvent -ea SilentlyContinue -FilterHashtable @{ProviderName='iotedged';LogName='application';StartTime=[datetime]::Now.AddSeconds(-$args[0])} |
     Select TimeCreated, Message |
     Sort-Object @{Expression='TimeCreated';Descending=$false} |
     Format-Table -AutoSize -Wrap
